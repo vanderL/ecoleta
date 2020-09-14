fb58379ec9d1f8ebc03a7bdbfa033701e85ee4b7
Vander Lima <vnd.vander@gmail.com>
2020-09-14 17:11:09 -0300

    Day 4.1 - Partials e Serve.js init

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const server =  express()
+
+//configurar a pasta publica
+server.use(express.static('public'))
+
+
+//enginer template
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+
+// rotas
+// configurar caminhos da minha aplicação
+// página inicial
+// req: requisição
+// res: Resposta
+server.get('/', (req, res) => {
+    return res.render('index.html'/*, { title: "Um titulo bem aqui novinho"}*/)
+})
+
+server.get('/create-point', (req, res) => {
+    return res.render('create-point.html'   )
+})
+
+server.get('/serach-results', (req, res) => {
+    return res.render('search-results.html')
+})
+
+//ligar o servidor
+server.listen(3000)
