a7496a3ca492908a902efb618fc56e72a52821a8
Vander Lima <vnd.vander@gmail.com>
2020-11-09 16:05:54 -0300

    function search

--- src/server.js
+++ src/server.js
@@ -81,8 +81,15 @@ server.post('/savepoint', (req, res) => {
 
 
 server.get('/search-results', (req, res) => {
+
+    const search = req.query.search
+
+    if(search == "") {
+        return res.render('search-results.html', { total: 0 })
+    }
+
     
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
